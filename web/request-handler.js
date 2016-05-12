0cbcf1915b5f0148f2814cb1f7d4de91ca4f2547
ashjd <ashjd29@gmail.com>
2016-05-11 20:10:13 -0700

    All tests pass

--- web/request-handler.js
+++ web/request-handler.js
@@ -6,22 +6,12 @@ var headers = require('./http-helpers.js');
 
 var handleRequest = function(request, response) {
   if (request.method === 'GET') {
-    if (request.url === '/') {
-      fs.readFile('./web/public/index.html', (err, data) => {
-        if (err) {
-          throw err;
-        }
-        statusCode = 200;
-        response.writeHead(statusCode, headers.headers);
-        response.end(data);
-      });
-    }
-    
+     
     archive.isUrlArchived (request.url.slice(1), function(data) {
       console.log (data, request.url.slice(1));
-      if (data) {
-        console.log('file path', archive.paths.archivedSites + request.url);
-        fs.readFile (archive.paths.archivedSites + request.url, (error, data) => {
+
+      if (request.url === '/') {
+        fs.readFile('./web/public/index.html', (err, data) => {
           if (err) {
             throw err;
           }
@@ -29,9 +19,38 @@ var handleRequest = function(request, response) {
           response.writeHead(statusCode, headers.headers);
           response.end(data);
         });
+      } else if (data) {
+        console.log('file path', archive.paths.archivedSites + request.url);
+        fs.readFile (archive.paths.archivedSites + request.url, 'utf8', (error, data) => {
+          console.log ('data : ', data);
+          if (error) {
+            throw error;
+          }
+          statusCode = 200;
+          response.writeHead(statusCode, headers.headers);
+          response.end(data);
+        });
+      } else {
+        statusCode = 404;
+        response.writeHead(statusCode, headers.headers);
+        response.end();
       }
     });    
-  }  
+  } else if (request.method === 'POST') {
+    request.setEncoding('utf8');
+    var postData = '';
+    request.on('data', function(chunk) {
+      postData += chunk;
+    });
+    request.on('end', function() {
+      console.log('postData:', postData.slice(4), ':end of postData');
+      archive.addUrlToList(postData.slice(4) + '\n', function() {
+        statusCode = 302;
+        response.writeHead(statusCode, headers.headers);
+        response.end();
+      });
+    });
+  } 
 };
 
 exports.handleRequest = handleRequest;
